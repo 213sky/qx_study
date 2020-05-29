138579ca3d330b7ea39dfe2906030882ac64c957
volcano <volcano1993@sina.com>
2020-05-29 22:55:36 +0800

    20200529练习

---
+++ src/day2020529/Practice01.java
@@ -0,0 +1,14 @@
+package day2020529;
+
+/**
+ * 使用循环输出九九乘法表，输出结果如下:
+ * 1 * 1 = 1
+ * 2 * 1 = 2, 2 * 2 = 4
+ * ...
+ * 9 * 1 = 9, 9 * 2 = 18, 9 * 3 = 27, ... 9 * 9 = 81
+ */
+public class Practice01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day2020529/Practice02.java
@@ -0,0 +1,16 @@
+package day2020529;
+
+/**
+ * 使用循环输出等腰三角形，例如给定4(4行，此值可变动)，输出结果如下:
+ */
+/*
+    #
+   ###
+  #####
+ #######
+ */
+public class Practice02 {
+    public static void main(String[] args) {
+
+    }
+}
