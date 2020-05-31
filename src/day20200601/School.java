ddd1f5a6dcb6d21b5dc10eb66968d63dec9073b3
213sky <volcano1993@sina.com>
2020-06-01 01:51:12 +0800

    20200601 类相关

---
+++ src/day20200601/School.java
@@ -0,0 +1,72 @@
+package day20200601;
+
+/**
+ * 利用student类，定义一个Student[]数组保存多个Student对象作为通讯录数据。
+ * 程序可以通过name、email、address查询，如果找不到数据，则进行友好提示。
+ * <p>
+ * 找出数据，可以全匹配，也可以模糊匹配(模糊匹配难做，就先做全匹配)
+ */
+public class School {
+    /**
+     * 保存Student[]不能用static修饰
+     */
+
+
+    public static void main(String[] args) {
+        //1. 初始化School类
+
+        //2. 添加学生，虚拟数据默认全部不重复，虚拟5-10个学生
+
+        //3. 按name查找学生，并显示学生信息name,email,address
+
+        //4. 按email查找学生，并显示学生信息name,email,address
+
+        //4. 按address查找学生，并显示学生信息name,email,address
+    }
+
+    /**
+     * @param name
+     * @return
+     */
+    public Student findStudentByName(String name) {
+        return null;
+    }
+
+    /**
+     * 按emil查找
+     *
+     * @param email
+     * @return
+     */
+    public Student findStudentByEmail(String email) {
+        return null;
+    }
+
+    /**
+     * 按address查找
+     * 具体方法自己写
+     * @param email
+     * @return
+     */
+
+    /**
+     * 按照name,email,address添加学生
+     *
+     * @param name
+     * @param email
+     * @param address
+     */
+    public void addStudent(String name, String email, String address) {
+
+    }
+
+    /**
+     * 添加学生
+     *
+     * @param student
+     */
+    public void addStudent(Student student) {
+
+    }
+
+}
---
+++ src/day20200601/Student.java
@@ -0,0 +1,9 @@
+package day20200601;
+
+/**
+ * 编写一个学生类，提供name,age,gnder,phone,address,email成员变量，且为每个成员变量提供setter,getter方法。
+ * 为学生类提供默认的构造器和带所有成员变量的构造器。
+ * 为学生类提供方法，用于描绘吃，喝，玩，睡等行为,方法内输出动作即可。
+ */
+public class Student {
+}
